53bb38bbcad9f2156ff5725068da107c48284ffd
bassam207 <bassamhassan191@gmail.com>
2023-06-15 23:35:19 +0300

    hello_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main-entry point of program
-*return-return zero meaning success 
+*main-entrypointofprogram
+*return-returnzeromeaningsuccess 
 */
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }

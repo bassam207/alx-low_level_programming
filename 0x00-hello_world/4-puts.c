9722128f01e599e245ff9e3a0628e256250962bc
bassam207 <bassamhassan191@gmail.com>
2023-06-15 23:47:09 +0300

    hello_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
-*main-entrypointofprogram
-*return-returnzeromeaningsuccess 
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+	return (0);
 }
+

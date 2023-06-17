fa3913a6fe1100d81cf85bf7cec5a852ae8a8dbe
bassam207 <bassamhassan191@gmail.com>
2023-06-17 18:30:19 +0300

    com

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print all possible combinations of single-digit numbers using putchar
+ *              and separate them by ", ".
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+
+		if (n == 57)
+			continue;
+
+		putchar(',');
+		putchar('\t');
+	}
+
+	putchar('\n');
+
+	return (0);
+}

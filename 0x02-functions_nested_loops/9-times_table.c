0c152dd6b6d38f147dadee8517331d4b66d09f54
bassam207 <bassamhassan191@gmail.com>
2023-06-21 02:21:51 +0300

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,9 +16,13 @@ void times_table(void)
 	if (product < 10)
 	{
 	_putchar(' ');
+	_putchar('0' + product);
 	}
+	else
+	{
 	_putchar(product / 10 + '0');
 	_putchar(product % 10 + '0');
+	}
 	if (column != 9)
 	{
 	_putchar(',');

b3138efbda23a2b01c15f84d1d9e65bbd4e9cda6
bassam207 <bassamhassan191@gmail.com>
2023-06-20 21:09:47 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - Prints lowercase alphabets from 'a' to 'z'
+*
+* Return: Always void
+*/
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+		_putchar('\n');
+	}
+}

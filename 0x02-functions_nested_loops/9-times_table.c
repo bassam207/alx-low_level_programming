0b9fe676ec6950cb1d634363428673b8c1652330
bassam207 <bassamhassan191@gmail.com>
2023-06-21 02:05:19 +0300

    taple

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* times_table - Prints the multiplication table from 0 to 9.
+*/
+void times_table(void)
+{
+	int row;
+	int column;
+
+	for (row = 0; row <= 9; row++)
+	{
+	for (column = 0; column <= 9; column++)
+	{
+	int product = row * column;
+
+	if (product < 10)
+	{
+	_putchar(' ');
+	}
+	else
+	{
+	_putchar(product / 10 + '0');
+	}
+	_putchar(product % 10 + '0');
+	if (column != 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+	_putchar('\n');
+	}
+}

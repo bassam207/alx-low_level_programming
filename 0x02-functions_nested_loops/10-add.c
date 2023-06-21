ca8df001e3f7b7a6628e9e7bf92c7142506469a1
bassam207 <bassamhassan191@gmail.com>
2023-06-21 03:54:01 +0300

    int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - add two integrs
+*
+* @a: first input
+* @b: second input
+*
+* Return: sum of a & b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}

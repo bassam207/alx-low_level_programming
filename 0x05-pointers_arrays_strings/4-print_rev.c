48f26477266a2f15b2d9d259ce1bf756840d156b
bassam207 <bassamhassan191@gmail.com>
2023-06-27 17:35:36 +0300

    reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_rev -  prints a string, in reverse
+*@s: the string
+*Return: void
+*/
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+	len++;
+	}
+	while (len--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+}

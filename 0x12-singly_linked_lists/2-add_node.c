e68f716c955e21fdc92c1d23e151c99d30ed4ee3
bassam207 <bassamhassan191@gmail.com>
2023-07-28 18:26:26 +0300

    bassam

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* add_node - add new node to linked list
+* @head: pointer to next node
+* @str: data value of node
+* Return: pointer ro first node
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	list_t *first_node;
+
+	first_node = (list_t *)malloc(sizeof(list_t));
+	}
+	if (first_node == NULL)
+		return (head);
+	{
+		first_node->str = str;
+		first_node->next = head;
+		head = first_node;
+	}
+	return (head);
+}

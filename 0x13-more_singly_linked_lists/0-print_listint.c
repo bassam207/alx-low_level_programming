e5e92d857b51035b7a6da8f801eeb49981ad7d64
bassam207 <bassamhassan191@gmail.com>
2023-08-02 01:28:27 +0300

    bassam

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all elemnts of linked list
+* @h: pointer to first node of linked list
+*Return: nodes number
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+	}
+	return (num);
+}

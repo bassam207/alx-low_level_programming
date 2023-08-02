37c7bb2cea885c9f9db3d240e6d784d59d56eec2
bassam207 <bassamhassan191@gmail.com>
2023-08-02 04:16:38 +0300

    b

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,9 +10,9 @@ size_t print_listint(const listint_t *h)
 	size_t i = 0;
 
 	if (h == NULL)
-		return (NULL);
+		return (0);
 
-	while (h)
+	while (h != NULL)
 	{
 	printf("%d\n", h->n);
 	h = h->next;

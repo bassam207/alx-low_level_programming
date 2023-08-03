7cae5778095df77d8e11f031795cb7a612fc9684
bassam207 <bassamhassan191@gmail.com>
2023-08-04 01:38:14 +0300

    b

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,9 +15,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		num = num * 2 + (b[i] - '0');
+		num = 2 * num + (b[i] - '0');
 	}
 	return (num);
 }

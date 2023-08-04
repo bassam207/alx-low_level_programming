1a539afb874db773408e6180c2c8b87787a748fa
bassam207 <bassamhassan191@gmail.com>
2023-08-04 19:32:55 +0300

    b

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* flib_bits - returns the number of bits you would
+*need to flip to get from one number to another
+* @n: first num
+* @m: second num
+* Return: number of bits to flib to convert number
+*/
+unsigned int flip_bits(unsigned long int n, unsignd long int m)
+{
+	unsigned long int xorv = n ^ m;
+	unsigned int count = 0;
+
+	while (xorv)
+	{
+		if (xorv & 1UL)
+			count++;
+		xorv = xorv >> 1;
+	}
+	return (count);
+}

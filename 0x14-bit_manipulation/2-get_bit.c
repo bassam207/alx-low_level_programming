abe1d9f187716b16c8142ee7a129f524679ce55a
bassam207 <bassamhassan191@gmail.com>
2023-08-04 19:10:17 +0300

    b

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
 * @index: index
 * Return: the bit, or -1
 */
-int get_bit(unsigned long int n, unsigned int index    )
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
